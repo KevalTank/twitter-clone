0846b519670215aa03dd5a740368ab94ea26cbeb
KevalTank <keval6488@gmail.com>
2023-01-30 22:46:07 +0530

    initial setup done

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,10 @@
+class AppWriteConstatnts {
+  // Database id
+  static const String databaseId = '63d7f05dd8f325d0134e';
+
+  // Project id
+  static const String projectId = '63d66ff594f3053f1f95';
+
+  // Endpoint which will help flutter app to communicate with appwrite
+  static const String endPoint = 'http://localhost:80/v1';
+}
